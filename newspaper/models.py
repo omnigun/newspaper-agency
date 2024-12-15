f7d5b18f03dfbc3b65095d98ad31e484e03336fc
Yuriy Komar <omnigun85@gmail.com>
2024-12-15 21:45:44 +0200

    add auth_user_model to settings.py and create migrations

--- newspaper/models.py
+++ newspaper/models.py
@@ -2,27 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Newspaper(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    published_date = models.DateField(auto_now=True)
-    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
-    publishers = models.ManyToManyField("Redactor", related_name="publishers")
-
-    class Meta:
-        ordering = ["published_date"]
-
-    def __str__(self):
-        return f"{self.title} ({self.published_date})"
-
-
-class Topic(models.Model):
-    name = models.CharField(max_length=120, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
 
@@ -34,3 +13,28 @@ class Redactor(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    published_date = models.DateField(auto_now=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    class Meta:
+        ordering = ["published_date"]
+
+    def __str__(self):
+        return f"{self.title} ({self.published_date})"
+
+
+
+
