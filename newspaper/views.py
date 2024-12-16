13a416aae3bf29e288fb448a8257ccbefdf32b16
Yuriy Komar <omnigun85@gmail.com>
2024-12-16 20:08:26 +0200

    add in views.py first view to main_page

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import Redactor, Topic, Newspaper
+
+@login_required
+def main_page(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
+
+
