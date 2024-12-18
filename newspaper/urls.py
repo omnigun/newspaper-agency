77efcd4fba1971f8cb2e7b3fbacd546ff111b980
Yuriy Komar <omnigun85@gmail.com>
2024-12-18 18:13:52 +0200

    add html template & stylize index.html

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -5,4 +5,5 @@ from newspaper.views import main_page
 app_name = "newspaper"
 urlpatterns = [
     path("", main_page, name="index"),
+
 ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from newspaper.models import Redactor, Topic, Newspaper
 
