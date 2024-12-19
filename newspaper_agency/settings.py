850e7b7a8406b8f65a553d36c7bb1d86f308ee4c
Yuriy Komar <omnigun85@gmail.com>
2024-12-19 23:22:32 +0200

    fix working log out procces

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -103,8 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "newspaper.Redactor"
-
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
